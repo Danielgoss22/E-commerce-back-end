4dad3494564b0388639b0ce06a4ed19eae1604c5
danielgoss22 <daniel.goss22@gmail.com>
2024-01-10 15:26:56 -0700

    set conditions and defined columns for models as well as began working on models index.js

--- models/index.js
+++ models/index.js
@@ -1,10 +1,24 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
 // Products belongsTo Category
+Category.hasMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_id",
+});
+Product.belongsToMany(Category, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: "tag_id",
+});
 
 // Categories have many Products
 
@@ -18,3 +32,23 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+// Traveller.belongsToMany(Location, {
+//   // Define the third table needed to store the foreign keys
+//   through: {
+//     model: Trip,
+//     unique: false
+//   },
+//   // Define an alias for when data is retrieved
+//   as: 'planned_trips'
+// });
+
+// Location.belongsToMany(Traveller, {
+//   // Define the third table needed to store the foreign keys
+//   through: {
+//     model: Trip,
+//     unique: false
+//   },
+//   // Define an alias for when data is retrieved
+//   as: 'location_travellers'
+// });
