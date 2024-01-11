1116645cf9aab14917a716f10c7e58b22b8b4a16
danielgoss22 <daniel.goss22@gmail.com>
2024-01-11 09:32:51 -0700

    updated and added hasMany and belongs to in models index

--- models/index.js
+++ models/index.js
@@ -33,45 +33,9 @@ Tags.belongsToMany(Product, {
 
 // Tags belongToMany Products (through ProductTag)
 
-// traveller_id: {
-//   type: DataTypes.INTEGER,
-//   references: {
-//     model: 'traveller',
-//     key: 'id',
-//     unique: false
-//   }
-// },
-// location_id: {
-//   type: DataTypes.INTEGER,
-//   references: {
-//     model: 'location',
-//     key: 'id',
-//     unique: false
-//   }
-
 module.exports = {
   Product,
   Category,
   Tag,
   ProductTag,
 };
-
-// Traveller.belongsToMany(Location, {
-//   // Define the third table needed to store the foreign keys
-//   through: {
-//     model: Trip,
-//     unique: false
-//   },
-//   // Define an alias for when data is retrieved
-//   as: 'planned_trips'
-// });
-
-// Location.belongsToMany(Traveller, {
-//   // Define the third table needed to store the foreign keys
-//   through: {
-//     model: Trip,
-//     unique: false
-//   },
-//   // Define an alias for when data is retrieved
-//   as: 'location_travellers'
-// });
